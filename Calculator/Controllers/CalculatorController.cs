c442e3c995befa7425831015daa42d7d2c7eee68
Zaigham Kashif <zaigham.kashif@contour-software.com>
2023-10-12 14:28:07 +0500

    first commit

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,74 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public static CalculatorModel calculator = new CalculatorModel();
+   
+        public ActionResult Index()
+        {
+            return View(calculator);
+        }
+
+        /*[HttpPost]
+        public ActionResult Index(CalculatorModel cal)
+        {
+            calculator.Number_1 = cal.Number_1;
+            calculator.Number_2 = cal.Number_2;
+
+            if (ModelState.IsValid)
+            {
+                calculator.Result = calculator.Number_1 + calculator.Number_2;
+            }
+            return View(calculator);
+        }*/
+
+        [HttpPost]
+        public ActionResult Update(string dataValue)
+        {
+            if (calculator.IsOperator(dataValue))
+            {
+                if (calculator.current_number != String.Empty)
+                {
+                    calculator.calculation_stack.Push(calculator.current_number);
+                    calculator.operator_stack.Push(dataValue);
+                    calculator.Current_string = calculator.Current_string + " " + dataValue + " ";
+                    calculator.current_number = string.Empty;
+                }
+                return Content(calculator.Current_string);
+            }
+            else if (calculator.IsEqual(dataValue))
+            {
+                if (calculator.current_number != String.Empty)
+                {
+                    calculator.calculation_stack.Push(calculator.current_number);
+                    calculator.current_number = String.Empty;
+                    calculator.Current_string = String.Empty;
+                }
+                calculator.CalculateResult();
+                return Content(calculator.Result);
+            }
+            else if (calculator.IsClear(dataValue))
+            {
+                calculator.current_number= String.Empty;
+                calculator.Result = String.Empty;
+                calculator.Current_string = String.Empty;   
+                calculator.operator_stack.Clear();
+                calculator.calculation_stack.Clear();
+                return Content(calculator.Current_string);
+            }
+            else
+            {
+                calculator.current_number += dataValue;
+                calculator.Current_string += dataValue;
+                return Content(calculator.Current_string);
+            }
+        }
+    }
+}
---
+++ Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public string Result { get; set; }
+        public string current_number { get; set; }
+        public Stack<string> calculation_stack { get; set; }
+        public Stack<string> operator_stack { get; set; }
+
+        public string Current_string { get; set; }
+        public CalculatorModel() {
+            calculation_stack = new Stack<string>();
+            operator_stack = new Stack<string>();   
+            current_number = String.Empty;
+            Result = String.Empty;
+        }
+
+        public bool IsOperator(string input)
+        {
+            if (input == "+" || input == "-" || input == "*" || input == "/")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsEqual(string input) 
+        {
+            if(input == "=")
+            { 
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsClear(string input)
+        {
+            if (input == "C")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void CalculateResult()
+        {
+            while (operator_stack.Count>0)
+            {
+                int number1 = Convert.ToInt32(this.calculation_stack.Pop());
+                int number2 = Convert.ToInt32(this.calculation_stack.Pop());
+                int result = 0;
+
+                string operation = this.operator_stack.Pop();
+                if (operation == "+")
+                {
+                    result = number2 + number1;
+                    calculation_stack.Push(Convert.ToString(result));
+                }
+                else if (operation == "-")
+                {
+                    result = number2 - number1;
+                    calculation_stack.Push(Convert.ToString(result));
+
+                }
+                else if (operation == "*")
+                {
+                    result = number2 * number1;
+                    calculation_stack.Push(Convert.ToString(result));
+
+                }
+                else if (operation == "/")
+                {
+                    result = number2 / number1;
+                    calculation_stack.Push(Convert.ToString(result));
+                }
+            }
+
+            Result = calculation_stack.Peek();
+        }
+    }
+}
